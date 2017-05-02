39fe42f1e7690aa22d8184436eaf31f318874e96
Nicholas Leacock <nicholas@swissmail.com>
2017-05-02 17:45:09 +0100

    Add full text search via Lunr module.

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.get('/', function (req, res) {
 app.post('/search', urlencodedParser, function(req, res) {
   var query = '%' + req.body.searchinput + '%';
   var results = db.any('SELECT * FROM weburlsandcontent WHERE description LIKE ${str}', {
-      str: query
+    str: query
     }).then(function (returned_data) {
       res.render('search_results', {data: returned_data, query: query});
     });
@@ -28,32 +28,33 @@ app.post('/search', urlencodedParser, function(req, res) {
 app.get('/testing', function(req, res) {
   var contentDB = db.any('SELECT * FROM weburlsandcontent', {
   }).then(function(all_data) {
-        var index = lunr(function () {
-          this.field('weburl');
-          this.field('title');
-          this.field('description');
-          this.field('keywords');
-          this.ref('id');
-          var store = {};
-          all_data.forEach(function (doc) {
-            this.add(doc);
-            store[doc.id] = { weburl: doc.weburl, title: doc.title, description: doc.description, keywords: doc.keywords };
-          }, this);
-          console.log(store[1].weburl);
-        });
-        // console.log(index) // MASSIVE object with 512 indexes and magnitude etc
-        console.log(index.search("HTML")[0].ref);
-        // idReference = index.search("HTML")[0].ref;
-        // console.log(idReference);
-        // console.log(all_data);
-        // .matchData.metadata.html.description
+    var store = {};
+    var index = lunr(function () {
+      this.field('weburl');
+      this.field('title');
+      this.field('description');
+      this.field('keywords');
+      this.ref('id');
+      all_data.forEach(function (doc) {
+        this.add(doc);
+        store[doc.id] = { weburl: doc.weburl, title: doc.title, description: doc.description, keywords: doc.keywords };
+      }, this);
+    });
+      idRef_array = index.search("web dev");
+      idRef_array.forEach(function (item) {
+        console.log(store[item.ref].weburl);
+        console.log(store[item.ref].title);
+        console.log(store[item.ref].description);
+        console.log(store[item.ref].keywords);
+});
+    // console.log(store)
   }).catch(function(e) {
     console.log(e);
   });
 });
 
 app.get('/about', function(req, res) {
-    res.render('about');
+  res.render('about');
 });
 
 app.listen(3000, function () {
