8953a32afc80b2cbe0e45905a45687635dc0dc14
Nicholas Leacock <nicholas@swissmail.com>
2017-05-02 18:22:14 +0100

    WIP Full text search

--- app.js
+++ app.js
@@ -17,41 +17,76 @@ app.get('/', function (req, res) {
 });
 
 app.post('/search', urlencodedParser, function(req, res) {
-  var query = '%' + req.body.searchinput + '%';
-  var results = db.any('SELECT * FROM weburlsandcontent WHERE description LIKE ${str}', {
-    str: query
-    }).then(function (returned_data) {
-      res.render('search_results', {data: returned_data, query: query});
+  var query = req.body.searchinput;
+  db.any('SELECT * FROM weburlsandcontent', {
+    }).then(function(all_data) {
+      var store = {};
+      var index = lunr(function () {
+        this.field('weburl');
+        this.field('title');
+        this.field('description');
+        this.field('keywords');
+        this.ref('id');
+        all_data.forEach(function (doc) {
+          this.add(doc);
+          store[doc.id] = { weburl: doc.weburl, title: doc.title, description: doc.description, keywords: doc.keywords };
+        }, this);
+      });
+        idRef_array = index.search(query);
+        console.log(idRef_array);
+        // res.render('search_results', {idRef_array: idRef_array, store: store, query: query});
+        // idRef_array.forEach(function (item) {
+          // console.log(store[item.ref].weburl);
+          // console.log(store[item.ref].title);
+          // console.log(store[item.ref].description);
+          // console.log(store[item.ref].keywords);
+  // });
+      // console.log(store)
+    }).catch(function(e) {
+      console.log(e);
     });
-});
-
-app.get('/testing', function(req, res) {
-  var contentDB = db.any('SELECT * FROM weburlsandcontent', {
-  }).then(function(all_data) {
-    var store = {};
-    var index = lunr(function () {
-      this.field('weburl');
-      this.field('title');
-      this.field('description');
-      this.field('keywords');
-      this.ref('id');
-      all_data.forEach(function (doc) {
-        this.add(doc);
-        store[doc.id] = { weburl: doc.weburl, title: doc.title, description: doc.description, keywords: doc.keywords };
-      }, this);
-    });
-      idRef_array = index.search("web dev");
-      idRef_array.forEach(function (item) {
-        console.log(store[item.ref].weburl);
-        console.log(store[item.ref].title);
-        console.log(store[item.ref].description);
-        console.log(store[item.ref].keywords);
-});
-    // console.log(store)
-  }).catch(function(e) {
-    console.log(e);
   });
-});
+
+
+
+
+
+// app.post('/search', urlencodedParser, function(req, res) {
+//   var query = '%' + req.body.searchinput + '%';
+//   var results = db.any('SELECT * FROM weburlsandcontent WHERE description LIKE ${str}', {
+//     str: query
+//     }).then(function (returned_data) {
+      // res.render('search_results', {data: returned_data, query: query});
+//     });
+// });
+
+// app.get('/testing', function(req, res) {
+//   var contentDB = db.any('SELECT * FROM weburlsandcontent', {
+//   }).then(function(all_data) {
+//     var store = {};
+//     var index = lunr(function () {
+//       this.field('weburl');
+//       this.field('title');
+//       this.field('description');
+//       this.field('keywords');
+//       this.ref('id');
+//       all_data.forEach(function (doc) {
+//         this.add(doc);
+//         store[doc.id] = { weburl: doc.weburl, title: doc.title, description: doc.description, keywords: doc.keywords };
+//       }, this);
+//     });
+//       idRef_array = index.search("web dev");
+//       idRef_array.forEach(function (item) {
+//         console.log(store[item.ref].weburl);
+//         console.log(store[item.ref].title);
+//         console.log(store[item.ref].description);
+//         console.log(store[item.ref].keywords);
+// });
+//     // console.log(store)
+//   }).catch(function(e) {
+//     console.log(e);
+//   });
+// });
 
 app.get('/about', function(req, res) {
   res.render('about');
